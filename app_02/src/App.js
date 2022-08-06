3a3db5ef9c35b8b276c8dcdfe6169a03eaf7819a
r.pouya <r.pouya@R-POUYA>
2022-08-06 15:16:27 +0430

    add initial-projects

---
+++ app_02/src/App.js
@@ -0,0 +1,15 @@
+const App = function () {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <div>
+        <h2>Title</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
